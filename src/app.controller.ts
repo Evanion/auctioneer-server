782e76aeb0f96210cce03bf2d4cd3d31e6b27d2b
Mikael Pettersson <evanion@Mikaels-MacBook-Pro.local>
2020-03-23 07:40:39 +0100

    add notice of cert to readme

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get, Inject, Redirect, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,6 +6,7 @@ export class AppController {
   @Inject() private readonly appService: AppService;
 
   @Get()
+  @Redirect('/graphql', HttpStatus.PERMANENT_REDIRECT)
   getHello(): string {
     return this.appService.getHello();
   }
